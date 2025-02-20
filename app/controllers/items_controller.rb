cb3d860294dab3ae9f2962efef9241a81cc02ae5
thisiscindy24 <cindyannwu@gmail.com>
2025-02-20 07:57:27 +0000

    redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,12 @@ class ItemsController < ApplicationController
   end
 
   def create_item
-    render({ :template => "item_templates/create" })
+    i = Item.new
+    i.link_url=params.fetch("link_url_param")
+    i.link_description=params.fetch("link_description_param")
+    i.thumbnail_url=params.fetch("thumbnail_url_param")
+    i.save
+    # render({ :template => "item_templates/create" })
+    redirect_to("/")
   end
 end
